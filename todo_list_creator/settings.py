2927bf93cfdfed59f884d597de06a6fbc0825873
Davide Laneri <davidelan@hotmail.com>
2024-07-21 16:50:21 +0000

    Add DATABASE_URL and SECRET_KEY

--- todo_list_creator/settings.py
+++ todo_list_creator/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7__!c1jau!5t3ey6z839!9+yks&&0+l253@$6ex%p9xslslldg'
+#SECRET_KEY = 'django-insecure-7__!c1jau!5t3ey6z839!9+yks&&0+l253@$6ex%p9xslslldg'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
