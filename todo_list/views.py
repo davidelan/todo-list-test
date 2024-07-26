023945d858f6fa3f0a2f747a219db75557b70ae8
Davide Laneri <davidelan@hotmail.com>
2024-07-26 16:04:24 +0000

    Add Update Task

--- todo_list/views.py
+++ todo_list/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 from django.urls import reverse_lazy
 
@@ -36,3 +36,9 @@ class TaskCreate(CreateView):
     model = Task
     fields = "__all__"
     success_urs = reverse_lazy('task')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('task')
