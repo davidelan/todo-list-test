d013f9e073433b17f8db887242c4b9015d9b7607
Davide Laneri <davidelan@hotmail.com>
2024-08-04 10:05:44 +0000

    Add base.html + static/style.css + index.html

--- todo_list_creator/settings.py
+++ todo_list_creator/settings.py
@@ -30,8 +30,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = False
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-davidelan-todolist-afzyxmofi01.ws.codeinstitute-ide.net"
